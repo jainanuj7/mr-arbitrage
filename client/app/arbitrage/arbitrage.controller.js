b7240d5e4d8e96c0fb107839539aeb41e00cf55a
Anuj Jain <lookforaj@gmail.com>
2018-06-25 20:38:05 +0530

    Changes in scope
    Scope of buy rates and sell rates

--- client/app/arbitrage/arbitrage.controller.js
+++ client/app/arbitrage/arbitrage.controller.js
@@ -6,6 +6,12 @@
   function arbitrageCtrl($scope, $http, ajaxService) {
     var self = this;
     console.log('arbitrageCtrl');
+    var zebpayBuyRates = new Object();
+    var zebpaySellRates = new Object();
+    
+    var binanceBuyRates = new Object();
+    var binanceSellRates = new Object();
+    
     var zebpayCryptos = [
       "BTC",
       "ETH",
@@ -50,13 +56,23 @@
     ];
 
     //fetchZebpay();
-    fetchBinance();
-    USDtoINR();
+    //fetchBinance();
+    //USDtoINR();
+    binanceBTCUSDT();
 
+    function binanceBTCUSDT() {
+      ajaxService.send('APIcaller', {
+          "url": "https://api.binance.com/api/v3/ticker/bookTicker?symbol=BTCUSDT"
+        }, 'POST')
+        .then(function (USDTResults) {
+          binanceBuyRates["BTC"] = USDTResults.data.askPrice;
+          binanceSellRates["BTC"] = USDTResults.data.bidPrice;
+          console.log($scope.BTCUSDT);
+        })
+
+    }
     //Fetch Zebpay price
     function fetchZebpay() {
-      var zebpayBuyRates = new Object();
-      var zebpaySellRates = new Object();
       for (var i = 0; i < zebpayCryptos.length; i++) {
         $http.get("https://www.zebapi.com/api/v1/market/ticker-new/" + zebpayCryptos[i] + "/inr")
           .then(function (zebpayResults) {
@@ -71,9 +87,6 @@
 
     //Fetch Binance price
     function fetchBinance() {
-
-      var binanceBuyRates = new Object();
-      var binanceSellRates = new Object();
       for (var i = 0; i < binanceCryptos.length; i++) {
         ajaxService.send('APIcaller', {
             "url": "https://api.binance.com/api/v3/ticker/bookTicker?symbol=" + binanceCryptos[i] + "BTC"
