cb643ba31086f7db7154287f4567e6c237dfb6c6
Anuj Jain <lookforaj@gmail.com>
2018-06-25 20:28:00 +0530

    Added USD to INR api
    Live USD-INR conversion API

--- client/app/arbitrage/arbitrage.controller.js
+++ client/app/arbitrage/arbitrage.controller.js
@@ -6,7 +6,7 @@
   function arbitrageCtrl($scope, $http, ajaxService) {
     var self = this;
     console.log('arbitrageCtrl');
-    var cryptos = [
+    var zebpayCryptos = [
       "BTC",
       "ETH",
       "BCH",
@@ -27,15 +27,38 @@
       "CMT",
       "NCASH"
     ];
+
+    var binanceCryptos = [
+      "ETH",
+      "BCC",
+      "LTC",
+      "XRP",
+      "EOS",
+      "OMG",
+      "TRX",
+      "GNT",
+      "ZRX",
+      "REP",
+      "KNC",
+      "BAT",
+      "VEN",
+      "AE",
+      "IOST",
+      "ZIL",
+      "CMT",
+      "NCASH"
+    ];
+
     //fetchZebpay();
     fetchBinance();
+    USDtoINR();
 
     //Fetch Zebpay price
     function fetchZebpay() {
       var zebpayBuyRates = new Object();
       var zebpaySellRates = new Object();
-      for (var i = 0; i < cryptos.length; i++) {
-        $http.get("https://www.zebapi.com/api/v1/market/ticker-new/" + cryptos[i] + "/inr")
+      for (var i = 0; i < zebpayCryptos.length; i++) {
+        $http.get("https://www.zebapi.com/api/v1/market/ticker-new/" + zebpayCryptos[i] + "/inr")
           .then(function (zebpayResults) {
             var name = zebpayResults.data.virtualCurrency;
             zebpayBuyRates[name] = zebpayResults.data.buy;
@@ -51,14 +74,31 @@
 
       var binanceBuyRates = new Object();
       var binanceSellRates = new Object();
-      for (var i = 0; i < cryptos.length; i++) {
-        ajaxService.send('binance', {
-            "url": "https://api.binance.com/api/v3/ticker/bookTicker?symbol="+ cryptos[i] +"BTC"
+      for (var i = 0; i < binanceCryptos.length; i++) {
+        ajaxService.send('APIcaller', {
+            "url": "https://api.binance.com/api/v3/ticker/bookTicker?symbol=" + binanceCryptos[i] + "BTC"
           }, 'POST')
-          .then(function (result) {
-            console.log(result);
+          .then(function (binanceResults) {
+            var name = binanceResults.data.symbol;
+            name = name.replace("BTC", "");
+            binanceBuyRates[name] = binanceResults.data.askPrice;
+            binanceSellRates[name] = binanceResults.data.bidPrice;
+            console.log(binanceBuyRates);
+            console.log(binanceSellRates);
           })
       }
     }
+
+    function USDtoINR() {
+      ajaxService.send('APIcaller', {
+          "url": "https://free.currencyconverterapi.com/api/v5/convert?q=USD_INR"
+        }, 'POST')
+        .then(function (USDtoINRResults) {
+          $scope.oneUSDtoINR = USDtoINRResults.data.results.USD_INR.val;
+          console.log($scope.oneUSDtoINR);
+        })
+    }
+
+    //$interval(USDtoINR, 3600000);
   }
 })();
--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,6 +1,6 @@
 var request = require('request');
 module.exports = function (app) {
-  app.post('/binance', function (req, res) {
+  app.post('/APIcaller', function (req, res) {
     request(req.body.url, function (error, response, body) { 
     res.send(body);
     });
